f350ce8a8ed8b095701f803be7aa151ef8e5c8b5
paidgeek <tin.rabzelj@gmail.com>
2016-03-09 09:36:36 +0100

    fix

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var app = require('../app');
+var app = require('./app');
 var debug = require('debug')('opkp-fitbit:server');
 var http = require('http');
 
-app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.PORT || 1337);
 
 var server = http.createServer(app);
 server.listen(app.get("port"));
